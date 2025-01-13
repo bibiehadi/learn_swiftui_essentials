e5b498a6b6c49a85a17760e4d39caf7480c4ce0f
bibiehadi <bibie.hadi22@gmail.com>
2025-01-13 23:22:23 +0700

    dynamic data list

--- Landmarks/Views/CircleImageView.swift
+++ Landmarks/Views/CircleImageView.swift
@@ -8,8 +8,9 @@
 import SwiftUI
 
 struct CircleImageView: View {
+    var image: Image
     var body: some View {
-        Image("turtlerock")
+        image
             .clipShape(Circle())
             .overlay{
                 Circle().stroke(.gray, lineWidth: 4)
@@ -19,5 +20,5 @@ struct CircleImageView: View {
 }
 
 #Preview {
-    CircleImageView()
+    CircleImageView(image: Image("turtlerock"))
 }
---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,47 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Bibie Hadi Kusuma on 13/01/25.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var landmark: Landmark
+    var body: some View {
+        ScrollView {
+            MapView(
+                coordinate: landmark.locationCoordinates
+            )
+                .frame(height: 300)
+            CircleImageView(
+                image: landmark.image
+            )
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+                HStack {
+                    Text(landmark.park)
+                    Spacer()
+                    Text(landmark.state)
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                Divider()
+                Text("About \(landmark.name)")
+                    .font(.title2)
+                Text(landmark.description)
+            }
+            .padding()
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    LandmarkDetail(landmark: landmarks[0])
+}
